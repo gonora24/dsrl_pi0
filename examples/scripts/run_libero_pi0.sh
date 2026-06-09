cfba09f8767f0c4e580bc192fdb772497eabc2e0
gonora24 <noragorhan@gmail.com>
2026-06-09 11:04:41 +0200

    feat: create scripts

---
+++ examples/scripts/run_libero_pi0.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1 
+#SBATCH --job-name=dsrl_pi0_libero_90_task2
+
+module load devel/miniforge
+conda deactivate
+conda activate dsrl_pi0
+export WANDB_API_KEY='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'
+export WANDB_EMAIL='noragorhan@gmail.com'
+export WANDB_USERNAME='noragorhan'
+export WANDB_TEAM='noragorhan-karlsruhe-institute-of-technology'
+proj_name=DSRL_pi0_Libero
+device_id=0
+wandb_mode=online  # online or offline
+export WANDB_MODE=${wandb_mode}
+
+export PYTHONPATH="${PYTHONPATH}:/pfs/data6/home/ka/ka_anthropomatik/ka_eu3660/projects/dsrl_pi0/"
+export PYTHONPATH=$PYTHONPATH:/pfs/data6/home/ka/ka_anthropomatik/ka_eu3660/projects/dsrl_pi0/LIBERO
+
+export DISPLAY=:0
+export MUJOCO_GL=egl
+export PYOPENGL_PLATFORM=egl  
+export MUJOCO_EGL_DEVICE_ID=$device_id
+
+export OPENPI_DATA_HOME=./openpi
+export EXP=./logs/$proj_name; 
+export CUDA_VISIBLE_DEVICES=$device_id
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+
+pip install mujoco==3.3.1
+pip install "transformers==4.53.2"
+
+python3 examples/launch_train_sim.py \
+--algorithm pixel_sac \
+--env libero \
+--prefix dsrl_pi0_libero \
+--suffix libero_90_task2 \
+--wandb_project ${proj_name} \
+--batch_size 256 \
+--discount 0.999 \
+--seed 0 \
+--max_steps 500000  \
+--eval_interval 10000 \
+--checkpoint_interval -1 \
+--log_interval 500 \
+--eval_episodes 10 \
+--multi_grad_step 20 \
+--start_online_updates 500 \
+--resize_image 64 \
+--action_magnitude 1.0 \
+--query_freq 20 \
+--hidden_dims 128 \
+--libero_suite "libero_90" \
+--libero_task_id 2 \
+--pi0_checkpoint openpi \
+--chunk_reward 0 \
+--use_chunky_actor_critic 0 \
+
+## batch size 256, multi grad step 20, hidden dims 50
