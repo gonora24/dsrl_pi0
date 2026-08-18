5e96d6e6913f119c3eba43646e90672f6cef10bb
gonora24 <noragorhan@gmail.com>
2026-08-18 11:08:00 +0200

    config: added new bash scripts

---
+++ examples/scripts/run_libero_pi05_criticgpt_aractor_diff_mean.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1 
+#SBATCH --job-name=pi05_l90_task12_diffmean7dims
+
+module load devel/miniforge
+conda deactivate
+conda activate dsrl_pi0
+export WANDB_API_KEY='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'
+export WANDB_EMAIL='noragorhan@gmail.com'
+export WANDB_USERNAME='noragorhan'
+export WANDB_TEAM='noragorhan-karlsruhe-institute-of-technology'
+proj_name=DSRL_pi0_Libero
+device_id=0
+wandb_mode=online  # online or offline
+export WANDB_MODE=${wandb_mode}
+
+export OUTPUT_DIR=/pfs/work9/workspace/scratch/ka_eu3660-rlinf_tmp/DSRL_pi0_Libero
+
+export PYTHONPATH="${PYTHONPATH}:/pfs/data6/home/ka/ka_anthropomatik/ka_eu3660/projects/dsrl_pi0/"
+export PYTHONPATH=$PYTHONPATH:/pfs/data6/home/ka/ka_anthropomatik/ka_eu3660/projects/dsrl_pi0/LIBERO
+
+export DISPLAY=:0
+export MUJOCO_GL=egl
+export PYOPENGL_PLATFORM=egl  
+export MUJOCO_EGL_DEVICE_ID=$device_id
+
+export OPENPI_DATA_HOME=./openpi
+export EXP=./logs/$proj_name; 
+export CUDA_VISIBLE_DEVICES=$device_id
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+export PYTHONUNBUFFERED=1
+
+pip install mujoco==3.3.1
+pip install "transformers==4.53.2"
+
+python3 examples/launch_train_sim.py \
+--algorithm pixel_sac \
+--env libero \
+--prefix dsrl_pi05_libero_90_task12 \
+--suffix criticgpt_aractor_diff_mean_7dims \
+--wandb_project ${proj_name} \
+--batch_size 256 \
+--discount 0.999 \
+--seed 0 \
+--max_steps 1000000 \
+--eval_interval 10000 \
+--checkpoint_interval 200000 \
+--log_interval 500 \
+--eval_episodes 10 \
+--multi_grad_step 20 \
+--start_online_updates 500 \
+--resize_image 64 \
+--action_magnitude 1.0 \
+--query_freq 10 \
+--hidden_dims 128 \
+--libero_suite "libero_90" \
+--libero_task_id 12 \
+--pi0_checkpoint pi05_libero \
+--critic_hidden_dims 512 256 128 \
+--chunk_reward 1 \
+--use_chunky_actor_critic 1 \
+--use_transformer_critic 1 \
+--transformer_n_embd 256 \
+--transformer_n_head 4 \
+--transformer_n_layer 3 \
+--transformer_weight_norm 1 \
+--transformer_use_bias 0 \
+--use_transformer_actor 1 \
+--actor_transformer_d_model 128 \
+--actor_transformer_n_layers 3 \
+--actor_transformer_n_heads 4 \
+--actor_transformer_dropout 0.0 \
+--use_actor_diff 0 \
+--marginalize_logprobs 0 \
+--clip_actor_grad_norm 1000.0 \
+--clip_critic_grad_norm 1000.0 \
+--use_actor_diff_mean 1 \
+--only_predict_dims_until 7 \
+
+## batch size 256, multi grad step 20, hidden dims 50, entropy scaling is auto
